0662d71b2ac263241fda9aa441f42f34866a81dd
Amanda Silva <amanda328@hotmail.com>
2024-04-19 12:55:10 +0100

    load data dynamically

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -53,6 +53,7 @@ export default function Ticket(){
             >
                 <Credential 
                     image={image} 
+                    data={badgeStorage.data}
                     onChangeAvatar={handleSelectImage} 
                     onExpandQRCode={() => setExpandQRCode(true)}
                 />
@@ -69,7 +70,7 @@ export default function Ticket(){
                 </Text>
                 
                 <Text className="text-white font-regular text-base mt-1 mb-6">
-                    Mostre ao mundo que você vai participar do Unite Summit!
+                    Mostre ao mundo que você vai participar do evento {badgeStorage.data.eventTitle}!
                 </Text>
 
                 <Button title="Compartilhar"/>
--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,15 +1,20 @@
 import { View, Image, ImageBackground, Text, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
+
+import { BadgeStorage } from "@/storage/badge-storage";
+
 import { colors } from "@/styles/colors";
+
 import { QRCode } from "@/components/qrcode";
 
 type Props = {
+    data: BadgeStorage,
     image?: string,
     onChangeAvatar?: () => void,
     onExpandQRCode?: () => void
 }
 
-export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props){
+export function Credential({ data, onChangeAvatar, onExpandQRCode, image }: Props){
     return(
         <View className="w-full self-stretch items-center">
              <Image 
@@ -24,8 +29,12 @@ export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props){
                     className="px-6 py-8 h-40 items-center self-stretch border-b border-white/10 overflow-hidden"
                 > 
                     <View className="w-full flex-row items-center justify-between">
-                        <Text className="text-zinc-50 text-sm font-bold">Unite summit</Text>
-                        <Text className="text-zinc-50 text-sm font-bold">#123</Text>
+                        <Text className="text-zinc-50 text-sm font-bold">
+                            {data.eventTitle}
+                        </Text>
+                        <Text className="text-zinc-50 text-sm font-bold">
+                            #{data.id}
+                        </Text>
                     </View>
 
                     <View className="w-40 h-40 bg-black rounded-full" />
@@ -54,11 +63,15 @@ export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props){
                     )
                 }
 
-                <Text className="font-bold text-2xl text-zinc-50 mt-4" >John Doe</Text>
+                <Text className="font-bold text-2xl text-zinc-50 mt-4" >
+                    {data.name}
+                </Text>
                 
-                <Text className="font-regular text-base text-zinc-300 mt-2 mb-4" >john@email.com</Text>
+                <Text className="font-regular text-base text-zinc-300 mt-2 mb-4" >
+                    {data.email}
+                </Text>
 
-                <QRCode value="teste" size={120} />
+                <QRCode value={data.checkInURL} size={120} />
 
                 <TouchableOpacity activeOpacity={0.7} className="mt-6" onPress={onExpandQRCode}>
                     <Text className="font-body text-orange-500 text-sm" >Ampliar QRCode</Text>
